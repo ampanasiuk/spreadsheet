e6b1cde0062f6abf8fa4b249d3072e1b1c583425
Adrian Panasiuk <4141848+ampanasiuk@users.noreply.github.com>
2021-12-01 21:53:39 +0100

    Add tests for mul, cache

--- src/spreadsheet/formula.py
+++ src/spreadsheet/formula.py
@@ -5,6 +5,15 @@ class formula:
     def __add__(self, other):
         raise NotImplementedError
 
+    def __radd__(self, other):
+        raise NotImplementedError
+
+    def __mul__(self, other):
+        raise NotImplementedError
+
+    def __rmul__(self, other):
+        raise NotImplementedError
+
 
 class ref(formula):
     def __init__(self, coord: CoordLike):
--- tests/test_spreadsheet.py
+++ tests/test_spreadsheet.py
@@ -1,5 +1,6 @@
 import pytest
 
+from spreadsheet import Coord
 from spreadsheet import Spreadsheet
 from spreadsheet import SpreadsheetError
 from spreadsheet.formula import ref
@@ -19,7 +20,7 @@ def test_cell_values_should_initially_be_zero(s, coord):
     assert 0 == s[coord]
 
 
-@pytest.mark.parametrize("coord", ["a1", "ab", "156", "A1B", "A1!"])
+@pytest.mark.parametrize("coord", ["A0", "A-1", "Ą1", "AAB", "a1", "ab", "156", "A1B", "A1!"])
 def test_should_only_accept_chess_coords(s, coord):
     with pytest.raises(SpreadsheetError, matches=r'invalid coord'):
         _ = s[coord]
@@ -58,7 +59,7 @@ def test_ref_should_follow_another_ref(s):
     assert -8 == s["B3"]
 
 
-def test_formula_sum_can_take_operands_ref_and_int(s):
+def test_formula_add_can_take_operands_ref_and_int(s):
     s["D8"] = ref("A2") + 1
     s["A2"] = 9
     assert 10 == s["D8"]
@@ -66,7 +67,15 @@ def test_formula_sum_can_take_operands_ref_and_int(s):
     assert 12 == s["D8"]
 
 
-def test_formula_sum_can_take_operands_ref_and_ref_and_ref(s):
+def test_formula_add_can_take_operands_int_and_ref(s):
+    s["D8"] = 1 + ref("A2")
+    s["A2"] = 9
+    assert 10 == s["D8"]
+    s["A2"] = 11
+    assert 12 == s["D8"]
+
+
+def test_formula_add_can_take_operands_ref_and_ref_and_ref(s):
     s["A1"] = ref("A2") + ref("A3") + ref("A4")
     s["A2"] = 1
     s["A3"] = 10
@@ -74,11 +83,34 @@ def test_formula_sum_can_take_operands_ref_and_ref_and_ref(s):
     assert 111 == s["A1"]
 
 
-def test_formula_sum_should_follow_refs(s):
+def test_formula_add_should_follow_refs(s):
     s["A1"] = ref("A2") + 1
     s["A2"] = ref("A3")
     s["A3"] = 10
     assert 11 == s["A1"]
+
+
+def test_formula_mul_can_take_operands_ref_and_int(s):
+    s["D8"] = ref("A2") * 2
+    s["A2"] = 9
+    assert 18 == s["D8"]
+    s["A2"] = 11
+    assert 22 == s["D8"]
+
+
+def test_formula_mul_can_take_operands_int_and_ref(s):
+    s["D8"] = 2 * ref("A2")
+    s["A2"] = 9
+    assert 18 == s["D8"]
+    s["A2"] = 11
+    assert 22 == s["D8"]
+
+
+def test_formula_mul_add_can_be_combined(s):
+    s["A1"] = (ref("A2") * 2 + 1) * ref("A3")
+    s["A2"] = 10
+    s["A3"] = 2
+    assert 42 == s["A1"]
 
 
 def test_formula_can_be_reused(s):
@@ -90,3 +122,30 @@ def test_formula_can_be_reused(s):
     assert 12 == s["A2"]
     assert 13 == s["A3"]
     assert 24 == s["A4"]
+
+
+def test_values_are_cached(s):
+    s["A1"] = 1
+    s["A2"] = ref("A1") + ref("A1") + ref("A1") + ref("A1") + ref("A1") + ref("A1") + ref("A1") + ref("A1")
+    s["A3"] = ref("A2") + ref("A2") + ref("A2") + ref("A2") + ref("A2") + ref("A2") + ref("A2") + ref("A2")
+    s["A4"] = ref("A3") + ref("A3") + ref("A3") + ref("A3") + ref("A3") + ref("A3") + ref("A3") + ref("A3")
+    s["A5"] = ref("A4") + ref("A4") + ref("A4") + ref("A4") + ref("A4") + ref("A4") + ref("A4") + ref("A4")
+    s["A6"] = ref("A5") + ref("A5") + ref("A5") + ref("A5") + ref("A5") + ref("A5") + ref("A5") + ref("A5")
+    s["A7"] = ref("A6") + ref("A6") + ref("A6") + ref("A6") + ref("A6") + ref("A6") + ref("A6") + ref("A6")
+    s["A8"] = ref("A7") + ref("A7") + ref("A7") + ref("A7") + ref("A7") + ref("A7") + ref("A7") + ref("A7")
+    s["A9"] = ref("A8") + ref("A8") + ref("A8") + ref("A8") + ref("A8") + ref("A8") + ref("A8") + ref("A8")
+    s["A10"] = ref("A9") + ref("A9") + ref("A9") + ref("A9") + ref("A9") + ref("A9") + ref("A9") + ref("A9")
+    s["A11"] = ref("A10") + ref("A10") + ref("A10") + ref("A10") + ref("A10") + ref("A10") + ref("A10") + ref("A10")
+    s["A12"] = ref("A11") + ref("A11") + ref("A11") + ref("A11") + ref("A11") + ref("A11") + ref("A11") + ref("A11")
+    s["A13"] = ref("A12") + ref("A12") + ref("A12") + ref("A12") + ref("A12") + ref("A12") + ref("A12") + ref("A12")
+    s["A14"] = ref("A13") + ref("A13") + ref("A13") + ref("A13") + ref("A13") + ref("A13") + ref("A13") + ref("A13")
+    s["A15"] = ref("A14") + ref("A14") + ref("A14") + ref("A14") + ref("A14") + ref("A14") + ref("A14") + ref("A14")
+    s["A16"] = ref("A15") + ref("A15") + ref("A15") + ref("A15") + ref("A15") + ref("A15") + ref("A15") + ref("A15")
+    s["A17"] = ref("A16") + ref("A16") + ref("A16") + ref("A16") + ref("A16") + ref("A16") + ref("A16") + ref("A16")
+    assert 281474976710656 == s["A17"]
+
+
+def test_diamond_should_recalc(s):
+    root = Coord("A1")
+    l = Coord("A2")
+    r = Coord("A3")
